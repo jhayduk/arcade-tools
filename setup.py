dadc58412a359ded04361de27de7c7ec9ee5464e
John Hayduk <jhayduk@cisco.com>
2024-12-27 17:19:23 -0500

    Reworking package structure to match those of the standard "requests" package in an effort to understand how this works since there is a working example for "requests" out there. This is a work in progress.

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name='arcade-tools',
     version='0.1.0',
-    packages=find_packages(),
+    packages=["arcade-tools"],
+    package_dir={"": "src"},
     install_requires=[
         'pygame==2.6.1'
     ],
